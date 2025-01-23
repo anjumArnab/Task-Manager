94baf4767ba9a917e756807ef72b0c2eaae5fdb7
anjumArnab <anjumarnab050@gmail.com>
2025-01-23 18:01:44 +0600

    minor changes

--- lib/screens/task_manager.dart
+++ lib/screens/task_manager.dart
@@ -11,6 +11,7 @@ class TaskManager extends StatefulWidget {
   const TaskManager({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _TaskManagerState createState() => _TaskManagerState();
 }
 
