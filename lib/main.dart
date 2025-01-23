3d5b427a939eb16b8b0e360c897bd6d91c19c646
anjumArnab <anjumarnab050@gmail.com>
2025-01-23 16:59:45 +0600

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,16 @@ import 'package:database_app/firebase_options.dart';
 import 'package:database_app/screens/task_manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options:DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const DBApp());
+  runApp(const Task());
 }
 
-class DBApp extends StatelessWidget {
-  const DBApp({super.key});
+class Task extends StatelessWidget {
+  const Task({super.key});
 
   @override
   Widget build(BuildContext context) {
